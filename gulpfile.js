d63ec32197f538c3350c8112eaf3f2fc9efa20fb
vsync <vsync.design@gmail.com>
2014-08-07 21:26:32 +0300

    -

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp            = require('gulp'),
+    gutil           = require('gulp-util'),
+    sass            = require('gulp-sass'),
+    cssGlobbing     = require('gulp-css-globbing'),
+    cmq             = require('gulp-combine-media-queries'),
+    autoprefixer    = require('gulp-autoprefixer');
+
+
+//////////////////////
+// CSS
+
+gulp.task('scss', function () {
+
+    gulp.src('css/dist/main.scss')
+        .pipe(cssGlobbing({ extensions:'.scss' }))
+        .pipe(sass({ errLogToConsole: true })) // { style:'expanded' }
+        .pipe(cmq())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./css'));
+
+});
